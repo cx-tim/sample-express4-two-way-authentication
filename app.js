1a49e07247f1e3e8f538d820b6bcfec032ebff6b
Tim Wong <cytimo@gmail.com>
2016-12-21 11:52:03 +0800

    add client authentication

--- app.js
+++ app.js
@@ -13,10 +13,17 @@ const app = express();
 // Express Setup
 
 app.all('*', function(req, res) {
-  if (req.secure)
-    res.send('Hello World!');
-  else
+  // Does the client connect with SSL encryption?
+  if (req.secure) {
+    // Is the client certificate authorized?
+    if (req.client.authorized)
+      res.send('Hope this simple demo helps you :)');
+    else
+      res.send('Unauthorized: you should provide a trusted certificate');
+  }
+  else {
     res.send('Please use HTTPs connection.');
+  }
 });
 
 
@@ -32,7 +39,16 @@ http
 https
   .createServer({
     key: fs.readFileSync('ssl/server.key'),
-    cert: fs.readFileSync('ssl/server.pem')
+    cert: fs.readFileSync('ssl/server.pem'),
+
+    // the magic for client authentication
+    requestCert: true,
+
+    // set this to `true` if unauthorized client should be rejected automatically
+    rejectUnauthorized: false,
+
+    // authorize the client certificate against our own CA
+    ca: [ fs.readFileSync('ssl/ca/demo.pem') ]
   }, app)
   .listen(HTTPS_PORT, function() {
     console.log('Secure Server listening on port %d', HTTPS_PORT);
